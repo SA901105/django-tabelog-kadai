789499fc0cc3cadbd4ba88152b1e53d9045a28a4
SA901105 <157731270+SA901105@users.noreply.github.com>
2024-10-09 02:20:29 +0900

    Update models.py with address freewords , budget, and region choice

--- userapp/models.py
+++ userapp/models.py
@@ -19,8 +19,8 @@ SCORE_CHOICES = [
     (5, '★★★★★'),
 ]
 
-# 住所の選択肢
-ADDRESS_CHOICES = [
+# 地域の選択肢
+REGION_CHOICES = [
     ('北区', '北区'),
     ('南区', '南区'),
     ('東区', '東区'),
@@ -37,11 +37,11 @@ BUDGET_CHOICES = [
 
 # 評価の選択肢
 RATING_CHOICES = [
-    (1, '1.0'),
-    (2, '2.0'),
-    (3, '3.0'),
-    (4, '4.0'),
-    (5, '5.0'),
+    (1, '★'),
+    (2, '★★'),
+    (3, '★★★'),
+    (4, '★★★★'),
+    (5, '★★★★★'),
 ]
 
 # 店舗情報
@@ -49,13 +49,13 @@ class Shop(models.Model):
     name = models.CharField("店舗名", max_length=255)
     pr_long = models.TextField("店舗紹介", blank=True, null=True)
     price_range = models.IntegerField("予算", choices=BUDGET_CHOICES, blank=True, null=True)  # 予算を選択式に変更
-    address = models.CharField("住所", max_length=255, choices=ADDRESS_CHOICES, blank=True, null=True)  # 住所を選択式に変更
+    address = models.CharField("住所", max_length=255, blank=True, null=True)  # フリーワード入力に変更
     tel = models.CharField("TEL", max_length=255, blank=True, null=True)
     opening_hours = models.CharField("営業時間", max_length=255, blank=True, null=True)
     regular_holiday = models.CharField("定休日", max_length=255, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="カテゴリ")
     description = models.TextField(blank=True, null=True)
-    region = models.CharField("地域", max_length=100, blank=True, null=True)  # 地域フィールドを追加
+    region = models.CharField("地域", max_length=10, choices=REGION_CHOICES, blank=True, null=True)  # 地域を選択式に変更
     rating = models.IntegerField("評価", choices=RATING_CHOICES, default=3)  # 評価を選択式に変更
 
     def __str__(self):
@@ -128,7 +128,7 @@ class Profile(models.Model):
     address = models.CharField(max_length=50, blank=True, verbose_name='住所')
     tel = models.CharField(max_length=20, blank=True, verbose_name='電話番号')
     birth_date = models.DateField(null=True, blank=True, verbose_name='誕生日')
-    business = models.CharField(max_length=20, blank=True, verbose_name='職業')  # maxlength -> max_length に修正
+    business = models.CharField(max_length=20, blank=True, verbose_name='職業')  # maxlength -> max_lengthに修正
     stripe_customer_id = models.CharField(max_length=255, blank=True)
     stripe_subscription_id = models.CharField(max_length=255, blank=True)
     stripe_card_name = models.CharField(max_length=255, blank=True)
@@ -147,3 +147,4 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     """Update user profile when user is saved"""
     instance.profile.save()
+
